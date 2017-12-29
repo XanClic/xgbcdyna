55af639155540736300d7bb26ae1290e019c51cb
Hanna Reitz <hanna@xanclic.moe>
2017-12-29 18:03:50 +0100

    Benutze den Precompiler für eine Menge Dinge
    (Aber noch nicht alle)

--- dynarec/vm-exit.c
+++ dynarec/vm-exit.c
@@ -1,34 +1,23 @@
+#define vmxvmappn(...) vmappn(opbuf, i, __VA_ARGS__, sizeof(__VA_ARGS__))
+
 static void exit_vm(uint8_t *opbuf, size_t *i, uint16_t exit_ip, uint8_t exit_cycles)
 {
     // 0x15 Bytes.
 
-    // mov word [vm_ip],exit_ip
-    vmapp2(opbuf, *i, 0xC766);
-    vmapp1(opbuf, *i, 0x05);
-    vmapp4(opbuf, *i, (uintptr_t)&vm_ip);
-    // mov byte [cycles_gone],exit_cycles
-    vmapp4(opbuf, *i, exit_ip | 0x05C60000);
-    vmapp4(opbuf, *i, (uintptr_t)&cycles_gone);
-    // jmp dword ei
-    vmapp2(opbuf, *i, exit_cycles | 0xE900);
-    vmapp4(opbuf, *i, (uint32_t)(CODE_EXITI - (*i + 4)));
+    vmxvmappn(@@asm("mov word [0x12345678],0x9abc", [0x78, 0x56, 0x34, 0x12], "(uintptr_t)&vm_ip", [0xbc, 0x9a], "exit_ip"));
+    vmxvmappn(@@asm("mov byte [0x12345678],0xff", [0x78, 0x56, 0x34, 0x12], "(uintptr_t)&cycles_gone", [0xff], "exit_cycles"));
+    size_t here = *i;
+    vmxvmappn(@@asm("jmp dword 0x12345678", [0x78 - 5, 0x56, 0x34, 0x12], "(uint32_t)(CODE_EXITI - (here + 5))"));
 }
 
 static void exit_vm_by_ret(uint8_t *opbuf, size_t *i, uint8_t exit_cycles)
 {
     // 0x1E Bytes.
 
-    // lahf; push eax; mov ax,[ebp]
-    vmapp4(opbuf, *i, 0x8B66509F);
-    // add bp,2
-    vmapp4(opbuf, *i, 0x83660045);
-    // mov word [vm_ip],ax
-    vmapp4(opbuf, *i, 0xA36602C5);
-    vmapp4(opbuf, *i, (uintptr_t)&vm_ip);
-    // pop eax; sahf; mov byte [cycles_gone],exit_cycles
-    vmapp4(opbuf, *i, 0x05C69E58);
-    vmapp4(opbuf, *i, (uintptr_t)&cycles_gone);
-    // jmp dword ei
-    vmapp2(opbuf, *i, exit_cycles | 0xE900);
-    vmapp4(opbuf, *i, (uint32_t)(CODE_EXITI - (*i + 4)));
+    vmxvmappn(@@asm("lahf \n push eax \n mov ax,[ebp] \n add bp,2"));
+    vmxvmappn(@@asm("mov [0x12345678],ax", [0x78, 0x56, 0x34, 0x12], "(uintptr_t)&vm_ip"));
+    vmxvmappn(@@asm("pop eax \n sahf"));
+    vmxvmappn(@@asm("mov byte [0x12345678],0xff", [0x78, 0x56, 0x34, 0x12], "(uintptr_t)&cycles_gone", [0xff], "exit_cycles"));
+    size_t here = *i;
+    vmxvmappn(@@asm("jmp dword 0x12345678", [0x78 - 5, 0x56, 0x34, 0x12], "(uint32_t)(CODE_EXITI - (here + 5))"));
 }
